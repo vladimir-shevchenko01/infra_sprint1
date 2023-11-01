968fd8f19a64d4858b43e6aeaccaffda4d43ba0a
Vladimir Shevchenko <prstud@yandex.ru>
2023-11-01 11:59:40 +0000

    infra

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.17.25', '127.0.0.1', 'localhost', 'prettycat.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,11 +90,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
@@ -110,3 +105,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
+
+STATIC_URL = '/static_back/'
+STATIC_ROOT = BASE_DIR / 'static_back'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/kittygram/media/'
